9fc43410504d6abf736417cb52c8f7b8fe572c62
Til Goepfert <tilgoepfert@gmail.com>
2023-11-17 18:03:05 +0100

    prepare grid search run

--- week_03/grid_search.py
+++ week_03/grid_search.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import math
@@ -61,32 +60,33 @@ grouped = df.groupby('batch')
 
 ##### GRID SEARCH #####
 
-batch_sizes = [32, 64, 128]
-lsmt_units = [64, 128, 256]
-sequence_lenghts = [32, 64, 128]
+batch_sizes = [64]
+lsmt_units = [128, 256]
+sequence_length = 256
 
-for batch_size, lsmt_unit_count, sequence_length in product(batch_sizes, lsmt_units, sequence_lenghts):
-  print(f"Training model with batch_size={batch_size}, lstm_units={lsmt_unit_count}, sequence_length={sequence_length}")
+# create sequences
+sequences = []
+sequence_labels = []
 
-  # create sequences
-  sequences = []
-  sequence_labels = []
+for batch, readings in grouped:
+  readings = readings.sort_values('zeit')
+  for i in range(0, len(readings) - sequence_length, sequence_length):
+    sequence = readings.iloc[i:i + sequence_length]
+    sequences.append(sequence[['zeit', 'sensorid', 'messwert']].values)
+    sequence_labels.append(sequence['label'].values[0])
 
-  for batch, readings in grouped:
-    readings = readings.sort_values('zeit')
-    for i in range(0, len(readings) - sequence_length, sequence_length):
-      sequence = readings.iloc[i:i + sequence_length]
-      sequences.append(sequence[['zeit', 'sensorid', 'messwert']].values)
-      sequence_labels.append(sequence['label'].values[0])
+# convert to numpy arrays
+sequences = np.array(sequences)
+sequence_labels = np.array(sequence_labels)
 
-  # convert to numpy arrays
-  sequences = np.array(sequences)
-  sequence_labels = np.array(sequence_labels)
+# split into train, test and validation data
+X_train, X_test, y_train, y_test = train_test_split(sequences, sequence_labels, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
-  # split into train, test and validation data
-  X_train, X_test, y_train, y_test = train_test_split(sequences, sequence_labels, test_size=0.2, random_state=42)
-  X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+# create a result dataframe to store the results
+results = []
 
+for batch_size, lsmt_unit_count in product(batch_sizes, lsmt_units):
   n_batches = math.ceil(len(X_train) / batch_size)
 
   model = Sequential()
@@ -96,8 +96,16 @@ for batch_size, lsmt_unit_count, sequence_length in product(batch_sizes, lsmt_un
 
   # callbacks
   cp_cb = ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, save_freq=8*n_batches)
-  stp_cb = EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True, min_delta=1e-4, start_from_epoch=32, verbose=1)
+  stp_cb = EarlyStopping(monitor='val_loss', patience=16, restore_best_weights=True, min_delta=1e-4, start_from_epoch=32, verbose=1)
 
   model.fit(X_train, y_train, epochs=256, batch_size=batch_size, callbacks=[cp_cb, stp_cb], validation_data=(X_val, y_val))
 
-  model.save(f'models/grid_search/lstm_{lsmt_unit_count}-seq_{sequence_length}-batch_{batch_size}.keras')
+  model.save(f'models/grid_search/lstm_{lsmt_unit_count}-batch_{batch_size}.keras')
+
+  loss, acc = model.evaluate(X_test, y_test, verbose=2)
+
+  # add results to result dataframe
+
+# write results
+df = pd.DataFrame(results)
+df.to_csv('grid_search_results.csv')
